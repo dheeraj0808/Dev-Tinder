7edfc4d67e05b419bd685503de4690c77f3bbd33
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-23 23:37:53 +0530

    understanding the property of routes and its behaviour

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.use("/user",(req,res)=>{
+app.use("/user", (req, res) => {
     console.log("User route");
     res.send("User route");
     //in the above line res.send is used to send the response to the client
     //and  if this line is not present the we got error 
-  
+
     //next () :- it is used to pass the request to the next middleware
 })
+
+app.get("/user",
+    (req, res, next) => {
+        console.log("this is the the first get");
+        next();
+    },
+    (req, res) => {
+        res.send("this is the final respose");
+
+    }
+
+);
 
 
 module.exports = app;
